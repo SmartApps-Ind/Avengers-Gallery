6ab34aebd640c1a9610e6d153a6e8061defdc251
Suman <iamsuman898@gmail.com>
2022-05-12 18:42:32 +0530

    commit of folder, deployed v1.1

---
+++ script.js
@@ -0,0 +1,8 @@
+// function for form submission 
+// I would suggest myself to not use any 3rd party app services for using form submissions unless I learn how to make custom backends with node and other required techs
+let navToggle = document.getElementById('nav-toggle');
+let hamburgerBtn = document.getElementById('hamburger');
+function menu(){
+    // navToggle.classList.togremgle('nav');
+    navToggle.classList.toggle('nav-active');
+}
